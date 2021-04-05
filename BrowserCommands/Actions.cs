ccdbf6ce9eb2e1d73b3ea1c666b94de40a12eb1d
Sagar <sagardaine14@gmail.com>
2021-04-06 00:32:55 +0530

    Added Action Class

---
+++ BrowserCommands/Actions.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.BrowserCommands
+{
+    public partial class Actions
+    {
+        public IWebDriver Driver;
+
+        public Actions(IWebDriver Driver) 
+        {
+            this.Driver = Driver;
+        }
+    }
+}
---
+++ BrowserCommands/JavaScriptActions.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.BrowserCommands
+{
+    public partial class Actions
+    {
+        public void JsVerifyPageCompleteState(double secondsToWait = 30) 
+        {
+            try
+            {
+                new WebDriverWait(Driver, TimeSpan.FromSeconds(secondsToWait)).Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+            }
+
+            catch 
+            {
+                if (!String.IsNullOrWhiteSpace(Driver.Url))
+                {
+                    throw new TimeoutException();
+                }
+
+                else 
+                {
+                    throw new TimeoutException();
+                }
+            }
+        }
+    }
+}
