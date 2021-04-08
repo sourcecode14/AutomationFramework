c8cede4379e933f8e189286e23eb4f60f4ce5ce1
Sagar <sagardaine14@gmail.com>
2021-04-09 02:28:13 +0530

    Added Serialiaze and Deserialiazed Methods

--- ApiAutomation/ApiHelper.cs
+++ ApiAutomation/ApiHelper.cs
@@ -9,6 +9,7 @@ namespace Automation.ApiAutomation
 {
     public class ApiHelper
     {
+        public ApiUtils ApiUtils { get; set; }
         public string ApiHost { get; set; }
         public Dictionary<string, string> Headers { get; set; }
 
@@ -16,6 +17,7 @@ namespace Automation.ApiAutomation
 
         public ApiHelper(string apiHost = "", Dictionary<string, string> headers = null, Dictionary<string, string> cookies = null) 
         {
+            ApiUtils = new ApiUtils();
             ApiHost = apiHost;
             Headers = headers;
             Cookies = cookies;
@@ -33,13 +35,33 @@ namespace Automation.ApiAutomation
 
             return restRequest;
         }
-        public IRestResponse SendRequest(Method method,string endpoint,JObject payLoad,string contentType= "application/json" ) 
+
+        public IRestResponse SendGetRequest(string endpoint , string queryparam=null) 
+        {
+            string endpointWithQueryparamIfAny = "";
+            if (String.IsNullOrWhiteSpace(queryparam))
+            {
+                endpointWithQueryparamIfAny = endpoint;
+            }
+            else 
+            {
+                endpointWithQueryparamIfAny = $"{endpoint}?{queryparam}";
+                
+            }
+            return SendRequest(Method.GET, endpointWithQueryparamIfAny);
+        }
+
+        public IRestResponse SendRequest(Method method,string endpoint,JObject payLoad=null,string contentType= "application/json" ) 
         {
             RestClient restClient = new RestClient(ApiHost);
             RestRequest restRequest = new RestRequest(endpoint, method);
             restRequest.AddHeaders(Headers);
             AddCookiesToRequest(restRequest);
-            restRequest.AddParameter(contentType,payLoad,ParameterType.RequestBody);
+            if (method != Method.GET) 
+            {
+                restRequest.AddParameter(contentType, payLoad, ParameterType.RequestBody);
+            }
+            
             IRestResponse restResponse = restClient.Execute(restRequest);
             return restResponse;
 
--- ApiAutomation/ApiUtils.cs
+++ ApiAutomation/ApiUtils.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,31 @@ namespace Automation.ApiAutomation
 {
     public class ApiUtils
     {
+        public JObject ConvertDatModelToJson(Object model) 
+        {
+            JObject payload = null;
+            try
+            {
+               return payload = JObject.Parse(JsonConvert.SerializeObject(model));        
+            }
 
+            catch(Exception e)
+            {
+                throw new Exception($"Failed to Convert {model.GetType().Name} Model to Json\nException Received as {e.Message}");
+            }
+        }
+
+        public T ConvertResponseToDataModel<T>(string responseBody) 
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(responseBody);
+            }
+
+            catch (Exception e) 
+            {
+                throw new Exception($"Failed to convert response string to Data Model Object");
+            }
+        }
     }
 }
